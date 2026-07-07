78dc502db946bb07c3273f1ae4591db8d90058bf
MohamedHafez20 <mohamedhafez0311@gmail.com>
2026-07-07 16:53:37 +0300

    Fix NavBar import casing

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from './NavBar';
 import SideBar from './SideBar';
 import Footer from './Footer';
 import BackgroundWrapper from './BackgroundWrapper';
